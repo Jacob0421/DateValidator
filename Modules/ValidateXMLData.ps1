1a29b185e4a16f129501f8cd4a795ebb7a8967c4
Jacob Roberts <jacobo0421@gmail.com>
2024-01-15 17:12:16 -0500

    Update ValidateXMLData.ps1
    Working XML Parser. Returning the correct status based on date provided in xml file.

--- Modules/ValidateXMLData.ps1
+++ Modules/ValidateXMLData.ps1
@@ -10,9 +10,29 @@ Function ValidateXMLFile ($Path, $AlertingPeriod) {
 
     $ExpiryRaw = [DateTime]::ParseExact($ConfigXML.Config.ExpiryDate, 'yyyy-MM-dd', $null)
     $GracePeriod = [int] (($ConfigXML.Config.GracePeriod) -replace "[^0-9]", "")
+    $ExpiryWithGrace = $ExpiryRaw.AddDays($GracePeriod)
 
-    Write-Host "$ExpiryRaw | $GracePeriod | $($ExpiryRaw.AddDays($GracePeriod))"
+    if (((Get-Date) -gt $ExpiryRaw) -and ((Get-Date) -lt $ExpiryWithGrace)) {
+        $Status = "On Grace Period"
+    }
+    elseif ((Get-Date) -gt $ExpiryRaw) {
+        $Status = "Expired"
+    }
+    elseif ((GetDate).AddMonths($AlertingPeriod) -gt $ExpiryRaw) {
+        $Status = "Nearing Expiry"
+    }
+    else {
+        $Status = "Good"
+    }
 
+    $Output = [PSCustomObject]@{
+        Status                = $Status
+        Expiry                = $ExpiryRaw
+        ExpiryWithGracePeriod = $ExpiryWithGrace
+        GracePeriod           = $GracePeriod
+    }
+
+    $Output
     
     Remove-Variable -Scope local -Name "*Expiry*", "*Grace*" -ErrorAction SilentlyContinue
 }
